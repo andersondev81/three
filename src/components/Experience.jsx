0759231d00e1299ecb597016f486724bc00faa11
Anderson Silva <andersonsilva88@gmail.com>
2025-04-20 20:52:22 -0300

    Test

---
+++ src/components/Experience.jsx
@@ -0,0 +1,11 @@
+import { Castle } from "../components/Castle"
+import React from "react"
+export const Experience = () => {
+  return (
+    <>
+      <mesh>
+        <Castle />
+      </mesh>
+    </>
+  )
+}
